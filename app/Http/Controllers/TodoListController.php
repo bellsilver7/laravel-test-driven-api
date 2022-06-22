5bcde788e3743a6e8c0df77a906645a8d396211c
bellsilver7 <bellsilver7@naver.com>
2022-06-22 12:29:43 +0900

    using factory

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TodoList;
 use Illuminate\Http\Request;
 
 class TodoListController extends Controller
 {
     public function index()
     {
+        $lists = TodoList::all();
         return response($lists);
     }
 }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,20 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_fetch_todo_list()
     {
         // preparation / prepare
+        $lists = TodoList::factory()->create(['name' => 'my list']);
+        // TodoList::create(['name' => 'my list']);
 
         // action / perform
         $response = $this->getJson(route('todo-list'));
 
         // assertion / predict
         $this->assertEquals(1, count($response->json()));
+        $this->assertEquals('my list', $response->json()[0]['name']);
     }
 }
