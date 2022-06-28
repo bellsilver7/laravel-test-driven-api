cd1649889139fb6eb517f2a168349d70227663e6
bellsilver7 <bellsilver7@naver.com>
2022-06-28 11:23:37 +0900

    Auth Register User

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('todo-list', TodoListController::class);
 Route::apiResource('todo-list.task', TaskController::class)->except('show')->shallow();
+
+Route::post('/register', RegisterController::class)->name('user.register');
