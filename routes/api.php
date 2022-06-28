735b140cedd7c78ef503cd67a8247ef7b9a10ad8
bellsilver7 <bellsilver7@naver.com>
2022-06-28 12:35:37 +0900

    Auth User Login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
@@ -9,3 +10,4 @@ Route::apiResource('todo-list', TodoListController::class);
 Route::apiResource('todo-list.task', TaskController::class)->except('show')->shallow();
 
 Route::post('/register', RegisterController::class)->name('user.register');
+Route::post('/login', LoginController::class)->name('user.login');
