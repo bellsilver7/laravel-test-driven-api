2e4940f685db673c1a2a7aace48cb8e1f6caf33e
bellsilver7 <bellsilver7@naver.com>
2022-06-30 18:03:02 +0900

    upload test file to google drive

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,56 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/drive', function () {
+    $client = new Google\Client();
+    $client->setClientId(env('GOOGLE_CLIENT_ID'));
+    $client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
+    $client->setRedirectUri('http://localhost:8000/google-drive/callback');
+    $client->setScopes([
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+    ]);
+    $url = $client->createAuthUrl();
+    return redirect($url);
+});
+
+Route::get('/google-drive/callback', function () {
+    $client = new Google\Client();
+    $client->setClientId(env('GOOGLE_CLIENT_ID'));
+    $client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
+    $client->setRedirectUri('http://localhost:8000/google-drive/callback');
+    $code = request('code');
+    $access_token = $client->fetchAccessTokenWithAuthCode($code);
+    return $access_token;
+});
+
+Route::get('/upload', function () {
+    try {
+        $client = new Google\Client();
+        $access_token = '{access_token}';
+        $client->setAccessToken($access_token);
+        $service = new Google\Service\Drive($client);
+        $file = new Google\Service\Drive\DriveFile();
+
+        DEFINE('TESTFILE', 'testfile-small.txt');
+        if (!file_exists(TESTFILE)) {
+            $fh = fopen(TESTFILE, 'w');
+            fseek($fh, 1024 * 1024);
+            fwrite($fh, '!', 1);
+            fclose($fh);
+        }
+
+        $file->setName('Hello World!');
+        $service->files->create(
+            $file,
+            [
+                'data' => file_get_contents(TESTFILE),
+                'mimeType' => 'application/octet-stream',
+                'uploadType' => 'multipart'
+            ]
+        );
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+});
