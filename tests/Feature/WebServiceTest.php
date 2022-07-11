a50d562ea58cb6b01f0a3cebfce4727e3df87976
bellsilver7 <bellsilver7@naver.com>
2022-07-11 18:03:29 +0900

    mock dependency injection

--- tests/Feature/WebServiceTest.php
+++ tests/Feature/WebServiceTest.php
@@ -21,16 +21,21 @@ class WebServiceTest extends TestCase
 
     public function test_a_user_can_connect_to_a_web_service_and_token_is_stored()
     {
-        $response = $this->getJson(route('web-service.connect', 'google-drive'))->assertOk()->json();
+        $this->mock(Client::class, function (MockInterface $mock) {
+            $mock->shouldReceive('setScopes');
+            $mock->shouldReceive('createAuthUrl')->andReturn('http://localhost');
+        });
+
+        $response = $this->getJson(route('web-service.connect', 'google-drive'))
+            ->assertOk()
+            ->json();
+        $this->assertEquals('http://localhost', $response['url']);
         $this->assertNotNull($response['url']);
     }
 
     public function test_web_service_callback_will_store_token()
     {
         $this->mock(Client::class, function (MockInterface $mock) {
-            $mock->shouldReceive('setClientId')->once();
-            $mock->shouldReceive('setClientSecret')->once();
-            $mock->shouldReceive('setRedirectUri')->once();
             $mock->shouldReceive('fetchAccessTokenWithAuthCode')
                 ->andReturn('fake-token');
         });
@@ -38,8 +43,6 @@ class WebServiceTest extends TestCase
         $this->postJson(route('web-service.callback'), ['code' => 'dummyCode'])
             ->assertCreated();
 
-        // access_token, id and secret
-        // token field, as a json
         $this->assertDatabaseHas('web_services', [
             'user_id' => $this->user->id,
             'token' => '{"access_token":"fake-token"}'
